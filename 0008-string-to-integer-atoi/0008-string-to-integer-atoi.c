82352a3e9cc31413700cfa03c178ee7a7de5ad7b
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-02 13:22:49 +0200

    Time: 5 ms (27.42%), Space: 5.8 MB (25.55%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.c
@@ -0,0 +1,41 @@
+#include <stddef.h>
+#include <string.h>
+
+int myAtoi(char* s) {
+    long	    num;
+	char        sign;
+
+	num = 0;
+	sign = 1;
+	while ((*s >= 9 && *s <= 13) || (*s == 32))
+		s++;
+	if (*s == '+' || *s == '-')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+    while (*s && *s == '0')
+        s++;
+	while (*s >= '0' && *s <= '9')
+	{
+        if (strlen(&*s) > 18)
+        {
+            if (sign == 1)
+                return (INT_MAX);
+            else
+                return (INT_MIN);
+        }
+		num *= 10;
+		num += (*s - '0');
+		s++;
+        if (*s < '0' && *s > '9')
+            return (num * sign);
+	}
+	num *= sign;
+    if (num > INT_MAX)
+        return (INT_MAX);
+    if (num < INT_MIN)
+        return (INT_MIN);
+    return (num);
+}
