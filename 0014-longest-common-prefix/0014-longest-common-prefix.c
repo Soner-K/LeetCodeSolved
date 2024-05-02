fe7e484a40d90c30ec900cd925f99954716f1e4b
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-02 15:44:51 +0200

    Time: 2 ms (58.34%), Space: 5.8 MB (52.06%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,40 @@
+#include <stdint.h>
+#include <string.h>
+
+void	move_shortest(char **strs, int strsSize)
+{
+	char	*tmp;
+	int8_t	i;
+	int8_t	min_index;
+
+	i = 1;
+	min_index = 0;
+	while (--strsSize)
+	{
+		if (strlen(strs[min_index]) > (int8_t)strlen(strs[i]))
+			min_index = i;
+		i++;
+	}
+	tmp = strs[0];
+	strs[0] = strs[min_index];
+	strs[min_index] = tmp;
+}
+
+char	*longestCommonPrefix(char **strs, int strsSize)
+{
+	int8_t	i;
+	int8_t	j;
+
+	i = 1;
+	j = 0;
+	move_shortest(strs, strsSize);
+	while (--strsSize)
+	{
+		while (strs[0][j] && strs[0][j] == strs[i][j])
+			j++;
+		strs[0][j] = '\0';
+		i++;
+		j = 0;
+	}
+	return (strs[0]);
+}
