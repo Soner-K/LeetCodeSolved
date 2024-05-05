55e1ccf4b76f49d57ae40f37c126206ab07ae9d3
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-05 14:07:41 +0200

    Time: 5 ms (20.15%), Space: 6.2 MB (25.87%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calPoints.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokaraku <sokaraku@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 13:05:01 by sokaraku          #+#    #+#             */
+/*   Updated: 2024/04/29 14:13:31 by sokaraku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_stack
+{
+	int				score;
+	struct s_stack	*next;
+}					t_stack;
+
+short int	tiny_atoi(char *s, char is_negative)
+{
+	short int	res;
+
+	res = 0;
+	if (*s == '-')
+		s++;
+	while (*s)
+	{
+		res *= 10;
+		res += (*s - '0');
+		s++;
+	}
+	if (is_negative)
+		return (res * -1);
+	return (res);
+}
+
+t_stack	*lstnew(int score)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->score = score;
+	return (new);
+}
+
+void	lst_push(t_stack **head, t_stack *new)
+{
+	new->next = *head;
+	(*head) = new;
+}
+
+int	calPoints(char **operations, int operationsSize)
+{
+	t_stack	*stack;
+	int		res;
+
+	stack = NULL;
+	res = 0;
+	while (operationsSize--)
+	{
+		if (*operations[0] == '+')
+		{
+			res = stack->score + stack->next->score;
+			lst_push(&stack, lstnew(res));
+		}
+		else if (*operations[0] == 'D')
+		{
+			res = stack->score * 2;
+			lst_push(&stack, lstnew(res));
+		}
+		else if (*operations[0] == 'C')
+			stack = stack->next;
+		else
+		{
+			res = tiny_atoi(*operations, *operations[0] == '-');
+			lst_push(&stack, lstnew(res));
+		}
+		operations++;
+	}
+	if (!stack)
+		return (0);
+	res = 0;
+	while (stack)
+	{
+		res += stack->score;
+		stack = stack->next;
+	}
+	return (res);
+}
