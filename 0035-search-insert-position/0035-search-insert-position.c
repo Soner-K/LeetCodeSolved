e2357fc40172e3f9e00ca8c3940653cd74292580
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-05 14:02:38 +0200

    Time: 2 ms (79.45%), Space: 5.7 MB (61.20%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,17 @@
+int	searchInsert(int *nums, int numsSize, int target)
+{
+	short int	left;
+
+	left = 0;
+    numsSize--;
+	while (numsSize >= left)
+	{
+        if (nums[(numsSize + left) / 2] == target)
+            return ((numsSize + left) / 2);
+		else if (nums[(numsSize + left) / 2] < target)
+			left = (numsSize + left) / 2 + 1;
+		else
+			numsSize = (numsSize + left) / 2 - 1;
+	}
+    return (left);
+} 
