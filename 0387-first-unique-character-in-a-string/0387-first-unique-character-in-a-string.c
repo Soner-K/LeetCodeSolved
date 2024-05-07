3db786f88f669e3dff56c1c1a386ea172f3e113c
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-07 16:07:43 +0200

    Time: 8 ms (76.67%), Space: 7.5 MB (31.17%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.c
@@ -0,0 +1,15 @@
+int	firstUniqChar(char *s)
+{
+    int   tab[26] = {0};
+    int   i = -1;
+
+    while (s[++i])
+        tab[(s[i] - 'a')]++;
+    i = -1;
+    while (s[++i])
+    {
+        if (tab[(s[i] - 'a')] == 1)
+            return (i);
+    }
+    return (-1);
+}  
