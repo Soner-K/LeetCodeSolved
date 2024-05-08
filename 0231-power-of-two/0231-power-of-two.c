2906ecd45f4ed10b518de629c1303feb9c4e911b
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-08 14:07:11 +0200

    Time: 0 ms (100.00%), Space: 5.7 MB (6.23%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n) {
+
+    if (n == 0)
+        return (false);
+    while (n)
+    {
+        if (n % 2 != 0 && n != 1)
+            return (false);
+        n /= 2;
+    }
+    return (true);
+}
