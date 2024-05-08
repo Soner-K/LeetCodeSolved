1d65008f315a3e61ed2258fc153dc4601a0e3a45
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-08 14:17:47 +0200

    Time: 10 ms (77.59%), Space: 6.3 MB (39.44%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.c
@@ -0,0 +1,11 @@
+bool isPowerOfThree(int n) {
+    if (n <= 0)
+        return (false);
+    while (n)
+    {
+        if (n % 3 != 0 && n != 1)
+            return (false);
+        n /= 3;
+    }
+    return (true);
+}
