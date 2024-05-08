38ae3285f0e0d8d96ebf9ede0c76e5081fa2c343
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-08 17:03:24 +0200

    Time: 109 ms (83.10%), Space: 26.5 MB (47.18%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.c
@@ -0,0 +1,36 @@
+typedef struct ListNode ListNode;
+
+struct ListNode* doubleIt(struct ListNode* head){
+	ListNode	*prev; 
+	ListNode	*itr;
+
+	if (head->val > 4)
+	{
+		prev = malloc(sizeof(ListNode));
+		prev->val = 1;
+		prev->next = head;
+		head = prev;
+        prev = head->next;
+        itr = head->next->next;
+	}
+    else
+    {
+	    prev = head;
+        itr = head->next;
+    }
+    prev->val = (prev->val * 2) % 10;
+    if (!itr)
+    {
+        return (head);
+    }
+	while (itr)
+	{
+		itr->val *= 2;
+		if (itr->val >= 10)
+			prev->val++;
+		itr->val %= 10;
+		itr = itr->next;
+		prev = prev->next;
+	}
+	return (head);
+}
