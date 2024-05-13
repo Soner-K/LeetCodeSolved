0f65b7e26470a2ff181fdee98428f28b4c206492
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-13 15:37:46 +0200

    Time: 5 ms (51.92%), Space: 6.2 MB (11.54%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
@@ -0,0 +1,33 @@
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    short int   med = 0;
+    uint8_t     row = 0;
+    uint8_t     left = 0;
+
+    while (med < matrixSize)
+    {
+        if (matrix[med][0] <= target && matrix[med][matrixColSize[med] - 1] >= target)
+        {
+            if (matrix[med][0] == target || matrix[med][matrixColSize[med] - 1] == target)
+                return (true);
+            else
+                break ;
+        }
+        else
+            med++;
+    }
+    if (med == matrixSize)
+        return (false);
+    row = med;
+    matrixSize = matrixColSize[row] - 1;
+    while (left <= matrixSize)
+    {
+        med = left + (matrixSize - left) / 2;
+        if (matrix[row][med] == target)
+            return (true);
+        else if (target < matrix[row][med])
+            matrixSize = med - 1;
+        else
+            left = med + 1;
+    }
+    return (false);
+} 
