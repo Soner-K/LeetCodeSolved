627fa24b7f14cd8d85cf3ae2daca13b3227209c2
Soner Karakus <137514038+Soner-K@users.noreply.github.com>
2024-05-17 17:59:20 +0200

    Time: 16 ms (9.09%), Space: 10.8 MB (77.27%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+#define OR 2
+#define AND 3
+
+bool evaluateTree(struct TreeNode* root) {
+    if (!root)
+        return (false);
+
+    if (root->val == true)
+        return (true);
+    else if (root->val == false)
+        return (false);
+    else if (root->val == OR)
+        return (evaluateTree(root->right) || evaluateTree(root->left));
+    else
+        return (evaluateTree(root->right) && evaluateTree(root->left));
+    return (false);
+}
